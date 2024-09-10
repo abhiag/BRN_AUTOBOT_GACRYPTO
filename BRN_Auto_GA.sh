a66df245a2c3e4b7ca63e6339558adfdd4f5db0d
abhiag <36300569+abhiag@users.noreply.github.com>
2024-09-10 15:36:14 +0530

    Update BRN_Auto_GA.sh

--- BRN_Auto_GA.sh
+++ BRN_Auto_GA.sh
@@ -6,8 +6,7 @@ sudo apt -qy upgrade
 sudo apt-get install nodejs npm -y
 sudo npm install n -g
 sudo n latest
-sudo npm install prompt-sync
-npm init
-npm install ethers@5
+sudo npm init
+sudo npm install ethers@5
 gh repo clone abhiag/BRN_AUTOBOT_GACRYPTO
 cd BRN_AUTOBOT_GACRYPTO
