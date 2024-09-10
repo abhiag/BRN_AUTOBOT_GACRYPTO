baa7217efe932c9d910670c9688f726b9507300a
abhiag <36300569+abhiag@users.noreply.github.com>
2024-09-10 15:46:57 +0530

    Update BRN_Auto_GA.sh

--- BRN_Auto_GA.sh
+++ BRN_Auto_GA.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-echo
-cd $HOME
+echo "Linux Package Updating"
 sudo apt -q update
 sudo apt -qy upgrade
+
+echo "Installing NodeJs & NPM"
 sudo apt-get install nodejs npm -y
 sudo npm install n -g
 sudo n latest
 sudo npm init
-sudo npm install ethers@5
+sudo npm install ethers@6
 gh repo clone abhiag/BRN_AUTOBOT_GACRYPTO
 cd BRN_AUTOBOT_GACRYPTO
+
+echo "running node index.js...." 
+node index.js
+echo "Setup complete! The Executor is now running."
+echo "Subscribe https://t.me/GaCryptOfficial"
