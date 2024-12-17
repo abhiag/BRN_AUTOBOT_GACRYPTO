1df00058f37b3d203de8767c2ece4d39629f55d9
abhiag <36300569+abhiag@users.noreply.github.com>
2024-12-17 14:02:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const sleepTimer = () => {
 }
 
 function generateRandomAmount() {
-    return parseFloat(Math.random() * (0.0150 - 0.0100) + 0.0100).toFixed(4);
+    return parseFloat(Math.random() * (0.10 - 0.11) + 0.10).toFixed(4);
 }
 
 function getRandomElement() {
@@ -72,7 +72,7 @@ const bridge = async (privateKey) => {
     let addrBridge = config.address
     let amountBridge = parseEther(generateRandomAmount())
     console.log(`bridge amount ${formatEther(amountBridge)} ETH`)
-    let netAmountBridge = amountBridge - (amountBridge * 0.01)
+    let netAmountBridge = amountBridge - (amountBridge * 0.1)
     console.log(`net amount ${formatEther(netAmountBridge.toString())}`)
     let chainId = getRandomElement()
     console.log(`bridge from ${config.name} to ${chainId}`)
